8fa2b3bab086485b49f748a479ee71d1cf9b8b29
Rodrigo Emanuel <drigosantos@gmail.com>
2020-10-14 18:56:53 -0300

    model e controller para upload e salvamento de imagens no banco de dados

--- backend/src/config/uploads.ts
+++ backend/src/config/uploads.ts
@@ -1,4 +1,3 @@
-import { request } from 'express';
 import multer from 'multer';
 import path from 'path';
 
@@ -6,10 +5,9 @@ export default {
     storage: multer.diskStorage({
         destination: path.join(__dirname, '..', '..', 'uploads'),
         filename: (request, file, cb) => {
-            const filename = `${Date.now}-${file.originalname}`;
+            const filename = `${Date.now()}-${file.originalname}`;
 
             cb(null, filename);
-        }
+        },
     })
 }
-
