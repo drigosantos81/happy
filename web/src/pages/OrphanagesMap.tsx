11553fc9e5aef7a84170aae43a7ee97023f1ec9e
Rodrigo Emanuel <drigosantos@gmail.com>
2020-10-14 21:17:03 -0300

    Incluido e redimensionado icone do mapa na web.

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet, { icon } from 'leaflet';
 
 import mapMarkerImg from '../images/mapmarker.svg';
 
 import '../styles/pages/orphanages-map.css';
 import 'leaflet/dist/leaflet.css';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
 
 function OrphanagesMap() {
     return (
@@ -31,6 +39,15 @@ function OrphanagesMap() {
                 style={{ width: '100%', height: '100%' }}
             >
                 <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+                <Marker
+                    icon={mapIcon}
+                    position={[-12.9567563,-38.497931]}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        OAF
+                    </Popup>
+                </Marker>
             </Map>
 
             <Link to="" className="create-orphanage">
