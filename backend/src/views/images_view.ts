3ed43a8b80bcbcb4bd14ad4ae95d01b1772c10b9
Rodrigo Emanuel <drigosantos@gmail.com>
2020-10-17 12:13:02 -0300

    Adicionado informacoes e imagens de um orfanato no mobile

--- backend/src/views/images_view.ts
+++ backend/src/views/images_view.ts
@@ -4,7 +4,7 @@ export default {
     render(image: Image) {
         return {
             id: image.id,
-            url: `http://localhost:3334/uploads/${image.path}`
+            url: `http://192.168.1.33:3334/uploads/${image.path}`
         };
     },
 
